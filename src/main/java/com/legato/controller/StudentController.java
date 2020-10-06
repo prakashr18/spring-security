1977fc914a187e851653e9e063378d20b6db32ea
Prakash Kumawat <Prakashkumawatr@gmail.com>
2020-10-06 11:03:37 +0530

    Initial version of spring security learning

---
+++ src/main/java/com/legato/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.legato.controller;
+
+import com.legato.dto.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Prakash Kumawat"),
+            new Student(2, "Vishal Sharma"),
+            new Student(3, "Naveen Kumar"),
+            new Student(4, "Chandra Kanth"),
+            new Student(5, "Vijay Kumar")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Student "+studentId +" does not exists"));
+    }
+}
